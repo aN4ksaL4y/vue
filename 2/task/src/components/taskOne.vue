0bd32545c1e74715b512c166d6d8b1ad1fc6c985
Your Name <you@example.com>
2024-10-11 12:33:06 +0700

    test input pake button

---
+++ 2/task/src/components/taskOne.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <input v-model="username" placeholder="Username here">
+        <ul>
+            <li v-for="(item, index) in items" :key="index">
+                {{ index }} {{ item }}
+            </li>
+            <button v-on:click="addItem">Add Item</button>
+        </ul>
+
+
+    </div>
+</template>
+<script>
+    export default {
+        data (){
+            return {
+                username:'',
+                items: ['item', 'item', 'item']
+            }
+        },
+        methods:{
+            addItem(){
+                this.items.push(this.username);
+            }
+        }
+    }
+</script>
